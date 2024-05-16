a014cbf12076cd68cfb936c7e14179670d58a85e
Himanshu-gupta31 <himanshu.gupta0031@gmail.com>
2024-05-17 02:41:19 +0530

    added comment,tweet,playlist and like model

---
+++ Backend/src/models/tweet.model.js
@@ -0,0 +1,12 @@
+import mongoose,{Schema} from "mongoose";
+const tweetSchema=new Schema({
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+    content:{
+        type:String,
+        required:true 
+    }
+},{timestamps:true})
+export const Tweet=mongoose.model("Tweet",tweetSchema)
--- src/utils/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-const asyncHandler = (requestHandler) => {
-  return (req, res, next) => {
-      Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
-  }
-}
-export {asyncHandler}
