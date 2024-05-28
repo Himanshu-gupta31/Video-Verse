f1d0530ace86d46711849e13183f5fcdf4d4cc0d
akshatg5 <akshatgirdhar5@gmail.com>
2024-05-29 00:58:29 +0530

    Sidebar changed

--- Frontend/src/components/Sidebar.tsx
+++ Frontend/src/components/Sidebar.tsx
@@ -1,18 +1,21 @@
 import React from "react";
-interface Sidebarprops{
-    SidebarTitle:string,
-    classname:string
+interface Sidebarprops {
+  SidebarTitle: string;
+  icon: JSX.Element;
 }
-export const Sidebar:React.FC<Sidebarprops>=({
-    SidebarTitle,
-    classname
-})=>{
-    return(
-        <>
-        <div className={`w-full p-2 h-8 text-white hover:bg-slate-900 hover:rounded-lg font-serif text-center ${classname}`} >
-            {SidebarTitle}
-            
-            </div>
-        </>
-    )
-}
+export const Sidebar: React.FC<Sidebarprops> = ({
+  SidebarTitle,
+  icon,
+}) => {
+  return (
+    <>
+      <div className="flex items-center p-2 my-2 hover:bg-slate-900 hover:rounded-lg w-[12rem]">
+        <div className="mx-2">{icon}</div>
+        <div className="text-white font-serif text-left"
+        >
+          {SidebarTitle}
+        </div>
+      </div>
+    </>
+  );
+};
