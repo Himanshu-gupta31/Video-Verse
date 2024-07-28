7c3d9483a5709d46b9acac91be29bebb1001234f
akshatg5 <akshatgirdhar5@gmail.com>
2024-07-29 02:31:27 +0530

    allowing all

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,16 +4,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
-const corsOrigin = process.env.CORS_ORIGIN || 'https://videoverse-two.vercel.app';
 
 const app = express();
-app.use(
-    cors({
-        origin: corsOrigin,
-        credentials: true,
-    })
-);
-
+app.use(cors());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 app.use(express.static("public"));
@@ -26,7 +19,7 @@ import playlistRouter from "./routes/playlist.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import likeRouter from "./routes/like.routes.js";
-import subscribeRouter from './routes/subscription.route.js';
+import subscribeRouter from "./routes/subscription.route.js";
 
 // Route declaration
 app.use("/api/v1/users", userRouter);
@@ -38,27 +31,34 @@ app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/subscribe", subscribeRouter);
 
 // default route
-app.get('/', (req, res) => {
-    res.send('Welcome to Video-Verse API!');
+app.get("/", (req, res) => {
+  res.send("Welcome to Video-Verse API!");
 });
 
 app.use((err, req, res, next) => {
-    console.error('Error details:', err);
-    console.error('Request details:', {
-      method: req.method,
-      url: req.url,
-      headers: req.headers,
-    });
-    
-    if (err.name === 'UnauthorizedError') {
-      return res.status(401).json({ error: 'Authentication error', details: 'Invalid or missing token' });
-    }
-    
-    if (err.message === 'Not allowed by CORS') {
-      return res.status(403).json({ error: 'CORS error: Origin not allowed' });
-    }
-    
-    res.status(err.status || 500).json({ error: 'Something went wrong!', details: err.message });
+  console.error("Error details:", err);
+  console.error("Request details:", {
+    method: req.method,
+    url: req.url,
+    headers: req.headers,
   });
-  
+
+  if (err.name === "UnauthorizedError") {
+    return res
+      .status(401)
+      .json({
+        error: "Authentication error",
+        details: "Invalid or missing token",
+      });
+  }
+
+  if (err.message === "Not allowed by CORS") {
+    return res.status(403).json({ error: "CORS error: Origin not allowed" });
+  }
+
+  res
+    .status(err.status || 500)
+    .json({ error: "Something went wrong!", details: err.message });
+});
+
 export { app };
