52d66247a3f82812e9391ed22e2dbaa54eda5fa8
Himanshu-gupta31 <himanshu.gupta0031@gmail.com>
2024-07-29 01:16:56 +0530

    app

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,8 +14,8 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ limit: "16kb" }));
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ limit: "20mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
