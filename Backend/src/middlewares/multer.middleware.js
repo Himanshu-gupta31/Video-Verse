d687060c0bc52d93102f91d742b89c4ec1c6a97b
akshatg5 <akshatgirdhar5@gmail.com>
2024-07-30 01:41:53 +0530

    vercel blob

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -3,7 +3,7 @@ import multer from "multer";
 const upload = multer({
   storage: multer.memoryStorage(),
   limits: {
-    fileSize: 16 * 1024 * 1024, // 5MB limit
+    fileSize: 16 * 1024 * 1024, // 16MB limit
   },
 });
 
