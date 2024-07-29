3e11f110c532a28a2da51a60158e951efc72635e
akshatg5 <akshatgirdhar5@gmail.com>
2024-07-30 00:38:26 +0530

    Multer change

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -1,16 +1,20 @@
 import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+// Ensure the directory exists
+const tempDir = path.join(__dirname, "public/temp");
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/temp")
-    },
-    filename: function (req, file, cb) {
-      
-      cb(null, file.originalname) //jab next time kuch improvement karna hoga toh file.koi aise use karunga jisse harr baar unique name aaye jisse files kabho overwrite naa hoo
-    }
-  })
-  
-  export const upload = multer(
-    { 
-        storage,
-     }
-)
+  destination: function (req, file, cb) {
+    cb(null, tempDir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // You can use a unique filename here if needed
+  }
+});
+
+export const upload = multer({ storage });
