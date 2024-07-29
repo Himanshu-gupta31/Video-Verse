b546a00ecd8d48c25c6d6d4445053c5c3a6d5397
akshatg5 <akshatgirdhar5@gmail.com>
2024-07-30 00:42:22 +0530

    Multer change

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -1,6 +1,11 @@
 import multer from "multer";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// Convert import.meta.url to __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Ensure the directory exists
 const tempDir = path.join(__dirname, "public/temp");
