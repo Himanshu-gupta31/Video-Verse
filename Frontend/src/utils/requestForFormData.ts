97a121afcf27231ed99733e4fa339b2fb61e77bf
Himanshu-gupta31 <himanshu.gupta0031@gmail.com>
2024-07-31 19:12:56 +0530

    fixed cors origin in requestform

--- Frontend/src/utils/requestForFormData.ts
+++ Frontend/src/utils/requestForFormData.ts
@@ -1,17 +1,12 @@
 // requestForFormData.ts
 import axios from "axios";
-import Cookies from "js-cookie";
+
 
 export const formDataRequest = axios.create({
-  baseURL: "https://video-verse-six.vercel.app/api/v1",
+  baseURL: "https://video-verse-4.onrender.com/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "multipart/form-data",
   },
 });
 
-// Interceptor to update the token before each request
-formDataRequest.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${Cookies.get("accesstoken")}`;
-  return config;
-});
