385e3cd515b228f974a19657e0fca11949d94c5a
akshatg5 <akshatgirdhar5@gmail.com>
2024-07-31 22:44:31 +0530

    Fixed dashboard number error

--- Backend/src/controllers/dashboard.controller.js
+++ Backend/src/controllers/dashboard.controller.js
@@ -1,49 +1,50 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Apierror } from "../utils/ApiError.js";
 import { Apisuccess } from "../utils/Apisuccess.js";
-import {Video} from "../models/video.model.js"
-import {Subscription} from "../models/subscription.model.js"
+import { Video } from "../models/video.model.js";
+import { Subscription } from "../models/subscription.model.js";
 import { isValidObjectId, mongoose } from "mongoose";
 const getChannelStatstotallikes = asyncHandler(async (req, res) => {
-    const userId = req.user?._id;
-  
-    if (!userId) {
-      throw new Apierror('User ID is required', 400);
-    }
-  
-    const videoLiked = await Video.aggregate([
-      {
-        $match: {
-          owner: new mongoose.Types.ObjectId(userId)
-        }
+  const userId = req.user?._id;
+
+  if (!userId) {
+    throw new Apierror("User ID is required", 400);
+  }
+
+  const videoLiked = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
       },
-      {
-        $lookup: {
-          from: "likes",
-          localField: "owner",
-          foreignField: "_id", 
-          as: "likes"
-        }
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "owner",
+        foreignField: "_id",
+        as: "likes",
       },
-      {
-        $addFields:{
-            Likes:{
-                $size:"$likes"
-            }
-        }
+    },
+    {
+      $addFields: {
+        Likes: {
+          $size: "$likes",
+        },
       },
-      {
-        $project:{
-            Likes:1
-        }
-      }
-    ])
-    if (!videoLiked || videoLiked.length === 0) {
-        throw new Apierror('No videos found for the given user', 404);
-      }
-    return res.status(200)
-    .json(new Apisuccess(200,"Videos liked",{videoLiked}))
-})
+    },
+    {
+      $project: {
+        Likes: 1,
+      },
+    },
+  ]);
+  if (!videoLiked || videoLiked.length === 0) {
+    throw new Apierror("No videos found for the given user", 404);
+  }
+  return res
+    .status(200)
+    .json(new Apisuccess(200, "Videos liked", { videoLiked }));
+});
 const getChannelStatusTotalVideos = asyncHandler(async (req, res) => {
   const userId = req.user?._id;
 
@@ -90,77 +91,106 @@ const getChannelStatusTotalVideos = asyncHandler(async (req, res) => {
     throw new Apierror(404, "No videos found for this user");
   }
 
-  return res.status(200).json(new Apisuccess(200, "Total video count fetched successfully", { totalVideos: totalVideos[0] }));
+  return res
+    .status(200)
+    .json(
+      new Apisuccess(200, "Total video count fetched successfully", {
+        totalVideos: totalVideos[0],
+      }),
+    );
 });
-const getChannelStatusTotalSubscriber=asyncHandler(async(req,res)=>{
-    const userId=req.user?._id
-    if(!userId){
-      throw new Apierror(400,"Invalid User Id")
-    }
-    const totalSubscriber=await Subscription.aggregate([
-      {
-        $match:{
-          channel:new mongoose.Types.ObjectId(userId)
-        }
-      },
-      {
-        $group:{
-          _id:"$channel",
-          totalSubscriber:{ $sum:1 }
-        }
-      },
-      {
-        $project:{
-          totalSubscriber:1
-        }
-      }
-    ])
-    if(!totalSubscriber || totalSubscriber.length===0){
-      throw new Apierror(404,"Total subscriber couldnt be fetched")
-    }
-    return res.status(200)
-    .json(new Apisuccess(200,"Total subscriber fetched successfully",{totalSubscriber}))
-
-})
-const getChannelVideos=asyncHandler(async(req,res)=>{
-  const userId=req.user?._id;
-  if(!isValidObjectId(userId)){
-    throw new Apierror(400,"Invalid User Id")
+const getChannelStatusTotalSubscriber = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+  if (!userId) {
+    throw new Apierror(400, "Invalid User Id");
   }
-  const channelVideo=await Video.aggregate([
-  {
-    $match:{
-      owner:new mongoose.Types.ObjectId(userId)
-    }
-  },
-  {
-    $lookup:{
-        from:"users",
-        localField:"owner",
-        foreignField:"_id",
-        as:"allvideos"
-    }
-  },
-  {
-    $addFields:{
-      totalvideo:{
-        $size:"$allvideos"
+  const totalSubscriber = await Subscription.aggregate([
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(userId),
       },
-    }
-  },
-  {
-    $project:{
-      allvideo:1,
-      totalvideo:1,
-      title:1,
-     }
+    },
+    {
+      $group: {
+        _id: "$channel",
+        totalSubscriber: { $sum: 1 },
+      },
+    },
+    {
+      $project: {
+        totalSubscriber: 1,
+      },
+    },
+  ]);
+  if (!totalSubscriber || totalSubscriber.length === 0) {
+    throw new Apierror(404, "Total subscriber couldnt be fetched");
   }
-])
+  return res
+    .status(200)
+    .json(
+      new Apisuccess(200, "Total subscriber fetched successfully", {
+        totalSubscriber,
+      }),
+    );
+});
+const getChannelVideos = asyncHandler(async (req, res) => {
+  try
+  {const userId = req.user?._id;
+  if (!isValidObjectId(userId)) {
+    throw new Apierror(400, "Invalid User Id");
+  }
+  const channelVideo = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "allvideos",
+      },
+    },
+    {
+      $addFields: {
+        totalvideo: {
+          $size: "$allvideos",
+        },
+      },
+    },
+    {
+      $project: {
+        allvideo: 1,
+        totalvideo: 1,
+        title: 1,
+      },
+    },
+  ]);
 
- if(!channelVideo || channelVideo.length===0){
-  throw new Apierror(404,"All Videos cannot be fetched or no video uploaded")
- }
- return res.status(200)
- .json(new Apisuccess(200,"All videos fetched successfully",{channelVideo}))
-})
-export {getChannelStatstotallikes,getChannelVideos,getChannelStatusTotalVideos,getChannelStatusTotalSubscriber}
+  if (!channelVideo || channelVideo.length === 0) {
+    throw new Apisuccess(410, {
+      numberOfVideos: 0,
+      message: "No videos uploaded by the user",
+    });
+  }
+  return res
+    .status(200)
+    .json(
+      new Apisuccess(200, "All videos fetched successfully", { channelVideo }),
+    );
+  } catch (error) {
+    return res
+    .status(200)
+    .json(
+      new Apisuccess(200, "All videos fetched successfully",error ),
+    );
+  }
+});
+export {
+  getChannelStatstotallikes,
+  getChannelVideos,
+  getChannelStatusTotalVideos,
+  getChannelStatusTotalSubscriber,
+};
--- Frontend/src/Pages/Dashboard.tsx
+++ Frontend/src/Pages/Dashboard.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { DashboardComponent } from "../components/DashboardComponent";
-import { useNavigate,Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { Menu } from "@headlessui/react";
 import Sidebarfull from "../components/Sidebarfull";
-import {newRequest} from "../utils/request.ts"
+import { newRequest } from "../utils/request.ts";
 
 const Dashboard: React.FC = () => {
   const [videoIds, setVideoIds] = useState<string[]>([]);
@@ -13,7 +13,7 @@ const Dashboard: React.FC = () => {
   const [totalSubs, setTotalSubs] = useState<any[]>([]);
   const navigate = useNavigate();
   const [userdetails, SetUserDetails] = useState<any>("");
-
+  const [error, setError] = useState<null | string>(null);
   const [channelId, setChannelId] = useState<string>("");
 
   useEffect(() => {
@@ -21,8 +21,7 @@ const Dashboard: React.FC = () => {
       try {
         if (videoIds.length > 0) {
           const response = await newRequest.get(
-            `/video/views/${videoIds.join(',')}`,
-            
+            `/video/views/${videoIds.join(",")}`
           );
           setTotalViews(response.data.data.totalViews);
         }
@@ -36,8 +35,7 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     const getUser = async () => {
       try {
-        const response = await newRequest.get("/users/getcurrentuser", 
-         );
+        const response = await newRequest.get("/users/getcurrentuser");
         // console.log("Current User Details", response.data);
         SetUserDetails(response.data.message);
       } catch (error) {
@@ -50,12 +48,15 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     const getChannelTotalVideos = async () => {
       try {
-        const response = await newRequest.get(
-          "/dashboard/videos"
-        );
+        const response = await newRequest.get("/dashboard/dashboard/videos");
         // console.log("Total Videos", response.data);
+        if (response.data.numberOfVideos == 0) {
+          setError("No Videos uploaded by you!");
+        }
         setTotalVideos(response.data.data.channelVideo);
-        setVideoIds(response.data.data.channelVideo.map((video: any) => video._id));
+        setVideoIds(
+          response.data.data.channelVideo.map((video: any) => video._id)
+        );
       } catch (error) {
         console.error("Error Fetching Total Videos", error);
       }
@@ -66,7 +67,8 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     const getUser = async () => {
       try {
-        const response = await newRequest.get("https://video-verse-six.vercel.app/api/v1/users/getcurrentuser"
+        const response = await newRequest.get(
+          "https://video-verse-six.vercel.app/api/v1/users/getcurrentuser"
         );
         // console.log("Current User Details", response.data);
         setUserDetails(response.data.message);
@@ -84,8 +86,7 @@ const Dashboard: React.FC = () => {
       try {
         if (channelId) {
           const response = await newRequest.get(
-            `/subscribe/getchannel/sub/${channelId}`,
-            
+            `/subscribe/getchannel/sub/${channelId}`
           );
           // console.log(response.data);
           setTotalSubs(response.data.data.subscriber);
@@ -98,106 +99,125 @@ const Dashboard: React.FC = () => {
   }, [channelId]);
 
   return (
-     <>
-     <div className="bg-black w-screen h-screen overflow-y-hidden flex">
-      {/* Sidebar */}
-      <hr className="absolute w-screen top-20 border border-t border-white"></hr>
-      <div className="absolute right-0 mt-4 mr-4 z-10">
-        {userdetails ? (
-          <Menu as="div" className="relative inline-block text-left">
-            <Menu.Button>
-              <img src={userdetails.avatar} className="rounded-full border border-white w-[3rem] h-[3rem]" />
-            </Menu.Button>
-            <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right bg-white border border-gray-300 divide-y divide-gray-100 rounded-md shadow-lg outline-none ">
-              <div className="px-1 py-1 ">
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      className={`${
-                        active ? "bg-gray-100" : "text-gray-900"
-                      } group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-indigo-500`}
-                      onClick={() => navigate("/video")}
-                    >
-                      Create Video
-                    </button>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      className={`${
-                        active ? "bg-gray-100" : "text-gray-900"
-                      } group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-indigo-500`}
-                      onClick={() => navigate("/createtweet")}
-                    >
-                      Create Tweet
-                    </button>
-                  )}
-                </Menu.Item>
-              </div>
-            </Menu.Items>
-          </Menu>
-        ) : (
-          <div>
-            <Link to="/signin">
-              <button className="border border-white text-white h-8 rounded-lg w-20 text-center ">Sign in</button>
-            </Link>
-          </div>
-        )}
-      </div>
+    <>
+      <div className="bg-black w-screen h-screen overflow-y-hidden flex">
+        {/* Sidebar */}
+        <hr className="absolute w-screen top-20 border border-t border-white"></hr>
+        <div className="absolute right-0 mt-4 mr-4 z-10">
+          {userdetails ? (
+            <Menu as="div" className="relative inline-block text-left">
+              <Menu.Button>
+                <img
+                  src={userdetails.avatar}
+                  className="rounded-full border border-white w-[3rem] h-[3rem]"
+                />
+              </Menu.Button>
+              <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right bg-white border border-gray-300 divide-y divide-gray-100 rounded-md shadow-lg outline-none ">
+                <div className="px-1 py-1 ">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? "bg-gray-100" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-indigo-500`}
+                        onClick={() => navigate("/video")}
+                      >
+                        Create Video
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? "bg-gray-100" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-indigo-500`}
+                        onClick={() => navigate("/createtweet")}
+                      >
+                        Create Tweet
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Menu>
+          ) : (
+            <div>
+              <Link to="/signin">
+                <button className="border border-white text-white h-8 rounded-lg w-20 text-center ">
+                  Sign in
+                </button>
+              </Link>
+            </div>
+          )}
+        </div>
 
-      <div className="flex-shrink-0 w-36 mt-2 max-sm:w-0">
-        <div className="mt-20">
-          <Sidebarfull />
+        <div className="flex-shrink-0 w-36 mt-2 max-sm:w-0">
+          <div className="mt-20">
+            <Sidebarfull />
+          </div>
+          <div className="border-t w-[17rem] bg-slate-500 max-sm:hidden"></div>
         </div>
-        <div className="border-t w-[17rem] bg-slate-500 max-sm:hidden"></div>
-        
+        <div className="flex-1 relative">
+          <div className="absolute left-32 top-20 bottom-0 bg-white border-l max-sm:hidden"></div>
+          <div className="flex justify-center mt-4">
+            <input
+              type="text"
+              placeholder="Search"
+              className="bg-gray-700 rounded-full w-[27rem] max-sm:w-[17rem] h-[3rem] text-left p-3 hover:outline outline-white"
+            />
+          </div>
+          <div className="mt-0 ml-[8.2rem]">
+            <div className="flex flex-col mt-1 ml-0">
+              {userDetails && (
+                <div className="mt-4 relative ">
+                  <img
+                    className="w-screen h-[15rem] object-cover"
+                    src={userDetails.coverimage}
+                    alt="Cover"
+                  />
+                  <img
+                    className="rounded-full border border-gray-400 w-[6rem] h-[6rem] absolute bottom-[-3rem] left-16"
+                    src={userDetails.avatar}
+                    alt="Avatar"
+                  />
+                </div>
+              )}
+              <div className="mt-16 ml-4">
+                {userDetails && (
+                  <p className="text-white">
+                    Welcome Back,
+                    <span className="text-blue-600 font-bold text-xl ml-2">
+                      {userDetails.username}
+                    </span>
+                  </p>
+                )}
+              </div>
+              <div className="grid grid-cols-3 ml-8 ">
+                <div className="w-[16rem] h-[6rem] border border-white text-center mt-8 text-white">
+                  <DashboardComponent heading="Total Subscriber" />
+                  <p>{totalSubs.length}</p>
+                </div>
+                <div className="w-[16rem] h-[6rem] border border-white text-center mt-8 text-white flex-col">
+                  <DashboardComponent heading="Total Videos" />
+                  <p>{totalVideos.length}</p>
+                </div>
+
+                <div className="w-[16rem] h-[6rem] border border-white text-center mt-8 text-white">
+                  <DashboardComponent heading="Total Views" />
+                  <p>{totalViews}</p>
+                </div>
+              </div>
+              {error && (
+                <div>
+                  <p>{error}</p>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="flex-1 relative">
-        <div className="absolute left-32 top-20 bottom-0 bg-white border-l max-sm:hidden"></div>
-        <div className="flex justify-center mt-4">
-          <input
-            type="text"
-            placeholder="Search"
-            className="bg-gray-700 rounded-full w-[27rem] max-sm:w-[17rem] h-[3rem] text-left p-3 hover:outline outline-white"
-          />
-        </div>
-        <div className="mt-0 ml-[8.2rem]">
-        <div className="flex flex-col mt-1 ml-0">
-          {userDetails &&
-            <div className="mt-4 relative ">
-              <img className="w-screen h-[15rem] object-cover" src={userDetails.coverimage} alt="Cover" />
-              <img className="rounded-full border border-gray-400 w-[6rem] h-[6rem] absolute bottom-[-3rem] left-16" src={userDetails.avatar} alt="Avatar" />
-            </div>
-          }
-          <div className="mt-16 ml-4">
-            {userDetails &&
-              <p className="text-white">Welcome Back,
-                <span className="text-blue-600 font-bold text-xl ml-2">{userDetails.username}</span>
-              </p>
-            }
-          </div>
-          <div className="grid grid-cols-3 ml-8 ">
-            <div className="w-[16rem] h-[6rem] border border-white text-center mt-8 text-white">
-              <DashboardComponent heading="Total Subscriber" />
-              <p>{totalSubs.length}</p>
-            </div>
-            <div className="w-[16rem] h-[6rem] border border-white text-center mt-8 text-white flex-col">
-              <DashboardComponent heading="Total Videos" />
-              <p>{totalVideos.length}</p>
-            </div>
-            
-            <div className="w-[16rem] h-[6rem] border border-white text-center mt-8 text-white">
-              <DashboardComponent heading="Total Views" />
-              <p>{totalViews}</p>
-            </div>
-          </div>
-        </div>
-        </div>
-        </div>
-        </div>
-        
-     </>
+    </>
   );
 };
 
