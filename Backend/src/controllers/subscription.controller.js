fbe2ebab1325977537e8a0c5ca54440aff3e5ecd
akshatg5 <akshatgirdhar5@gmail.com>
2024-07-31 23:04:53 +0530

    Fixing dashboards

--- Backend/src/controllers/subscription.controller.js
+++ Backend/src/controllers/subscription.controller.js
@@ -66,57 +66,70 @@ const togglesSubscription = asyncHandler(async (req, res) => {
   }
 });
 
-
 const getChannelSubscriber = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
+
   if (!isValidObjectId(channelId)) {
     throw new Apierror(400, "Channel id is invalid");
   }
-  const subscriber = await Subscription.aggregate([
-    {
-      $match: {
-        channel: new mongoose.Types.ObjectId(channelId),
-      },
-    },
-    {
-      $lookup: {
-        from: "users",
-        localField: "subscriber",
-        foreignField: "_id",
-        as: "subscribercount",
-      },
-    },
-    {
-      $addFields: {
-        subs: {
-          $size: "$subscribercount",
-        },
-      },
-    },
-    {
-      $project: {
-        subs: 1,
-        subscribercount: {
-          _id: 1,
-          fullname: 1,
-          username: 1,
-        },
-      },
-    },
-  ]);
-  if (!subscriber || subscriber.length === 0) {
-    throw new Apisuccess(201, {
-      numberOfSubscribers : 0,
-      message : 'Correctly fetching subscriber count!'
-    });
-  }
-  return res.status(200).json(
-    new Apisuccess(200, "All subscribed fetched successfully", {
-      subscriber,
-    }),
-  );
-});
 
+  try {
+    const subscriber = await Subscription.aggregate([
+      {
+        $match: {
+          channel: new mongoose.Types.ObjectId(channelId),
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "subscriber",
+          foreignField: "_id",
+          as: "subscribercount",
+        },
+      },
+      {
+        $addFields: {
+          subs: {
+            $size: "$subscribercount",
+          },
+        },
+      },
+      {
+        $project: {
+          subs: 1,
+          subscribercount: {
+            _id: 1,
+            fullname: 1,
+            username: 1,
+          },
+        },
+      },
+    ]);
+
+    if (!subscriber || subscriber.length === 0) {
+      return res.status(200).json(
+        new Apisuccess(
+          201,
+          {subscriber},
+          {
+            numberOfSubscribers : 0,
+            message : '0 Subscribers'
+          }
+        ),
+      );
+    }
+
+    return res.status(200).json(
+      new Apisuccess(200, "All subscribers fetched successfully", {
+        subscriber,
+      }),
+    );
+  } catch (error) {
+    console.error("Error fetching subscribers:", error);
+    throw new Apierror(500, "Something went wrong!");
+  }
+});
 
 const getSubscribedChannel = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
